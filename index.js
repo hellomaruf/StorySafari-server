50b8fc2306e7d743382f168d7d091a6266ea0725
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-10 21:06:21 +0600

    Create data in mongo database

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.0o9qayn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,6 +28,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    const booksCollection = client.db("storySafari").collection("books");
+
+    // create data in database
+    app.post("/books", async (req, res) => {
+      const books = req.body;
+      const result = await booksCollection.insertOne(books);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -33,7 +47,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("StorySafari server is running!");
 });
 
 app.listen(port, () => {
