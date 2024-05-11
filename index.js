c76f8ab0f7bdda1da2d78c6cb6889ef57fbdc16e
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-12 02:20:58 +0600

    Added some problem solving

--- index.js
+++ index.js
@@ -91,7 +91,12 @@ async function run() {
       res.send(result);
     });
 
-
+    app.delete("/borrowedBook/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await borrowCollection.deleteOne(query);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
